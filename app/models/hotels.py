36fa8020c6a38c895b65a4891256961e7524a9f5
flomasta <zatevi@mail.ru>
2024-06-01 09:43:13 +0300

    create: model hotels

---
+++ app/models/hotels.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, JSON
+
+from app.core.database import Base
+
+
+class Hotels:
+    __tablename__ = 'hotel'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    services = Column(JSON)
+    room_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
